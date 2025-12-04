f76f786770a72a642d50c752a49348f7cc728b30
MadeInShineA <olivier.amacker@netplus.ch>
2025-12-04 17:32:35 +0100

    DOC: Started working on the gqi predict example documentation

---
+++ doc/examples/reconst_gqi_predict.py
@@ -0,0 +1,11 @@
+"""
+===============================================
+Data cleaning with Generalized Q-Sampling Imaging
+===============================================
+
+We show how to use Generalized Q-Sampling Imaging :footcite:p:`Yeh2010`
+to diffusion MRI datasets. You can think of GQI as an analytical version of
+DSI orientation distribution function (ODF) (Garyfallidis, PhD thesis, 2012).
+
+First import the necessary modules:
+"""
